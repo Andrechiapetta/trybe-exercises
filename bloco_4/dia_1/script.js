8f027eb2e36cb8b91a26a6f6de168a7e6d78489f
Andrechiapetta <andreluizchiapetta@gmail.com>
2020-11-25 18:39:43 -0300

    Terminado exercício 1

--- bloco_4/dia_1/script.js
+++ bloco_4/dia_1/script.js
@@ -1,5 +1,29 @@
 function adicao () {
   let a = 5;
   let b = 10;
-  alert('function adicao () {\n  let a = 5;\n  let b = 10;\n  a + b = ' + (a + b) + '\n }');
+  alert('function adicao () {\n  let a = 5;\n  let b = 10;\n  a + b = ' + (a + b) + '\n}');
+}
+
+function subtracao () {
+  let a = 10;
+  let b = 5;
+  alert('function subtracao () {\n  let a = 10;\n  let b = 5;\n  a - b = ' + (a - b) + '\n}');
+}
+
+function multiplicacao () {
+  let a = 10;
+  let b = 5;
+  alert('function multiplicacao () {\n  let a = 10;\n  let b = 5;\n  a * b = ' + (a * b) + '\n}');
+}
+
+function divisao () {
+  let a = 10;
+  let b = 5;
+  alert('function divisao () {\n  let a = 10;\n  let b = 5;\n  a / b = ' + (a / b) + '\n}');
+}
+
+function modulo () {
+  let a = 10;
+  let b = 5;
+  alert('function modulo () {\n  let a = 10;\n  let b = 5;\n  a % b = ' + (a % b) + '\n}');
 }
