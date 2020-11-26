890afe3c0ae1eb29be61652f9ad720922bed0487
Andrechiapetta <andreluizchiapetta@gmail.com>
2020-11-26 00:06:22 -0300

    Terminado exercício 4

--- bloco_4/dia_1/script.js
+++ bloco_4/dia_1/script.js
@@ -1,34 +1,34 @@
-function adicao () {
+function adicao() {
   let a = 5;
   let b = 10;
-  alert('function adicao () {\n  let a = 5;\n  let b = 10;\n  a + b = ' + (a + b) + '\n}');
+  alert('function adicao() {\n  let a = 5;\n  let b = 10;\n  a + b = ' + (a + b) + '\n}');
 }
 
-function subtracao () {
+function subtracao() {
   let a = 10;
   let b = 5;
-  alert('function subtracao () {\n  let a = 10;\n  let b = 5;\n  a - b = ' + (a - b) + '\n}');
+  alert('function subtracao() {\n  let a = 10;\n  let b = 5;\n  a - b = ' + (a - b) + '\n}');
 }
 
-function multiplicacao () {
+function multiplicacao() {
   let a = 10;
   let b = 5;
-  alert('function multiplicacao () {\n  let a = 10;\n  let b = 5;\n  a * b = ' + (a * b) + '\n}');
+  alert('function multiplicacao() {\n  let a = 10;\n  let b = 5;\n  a * b = ' + (a * b) + '\n}');
 }
 
-function divisao () {
+function divisao() {
   let a = 10;
   let b = 5;
-  alert('function divisao () {\n  let a = 10;\n  let b = 5;\n  a / b = ' + (a / b) + '\n}');
+  alert('function divisao() {\n  let a = 10;\n  let b = 5;\n  a / b = ' + (a / b) + '\n}');
 }
 
-function modulo () {
+function modulo() {
   let a = 10;
   let b = 5;
-  alert('function modulo () {\n  let a = 10;\n  let b = 5;\n  a % b = ' + (a % b) + '\n}');
+  alert('function modulo() {\n  let a = 10;\n  let b = 5;\n  a % b = ' + (a % b) + '\n}');
 }
 
-function maiorDeDois () {
+function maiorDeDois() {
   let maior;
   let a = 10;
   let b = 5;
@@ -37,10 +37,10 @@ function maiorDeDois () {
   } else {
     maior = b;    
   }
-  alert('function maiorDeDois () {\n  let maior;\n  let a = 10;\n  let b = 5;\n\n  if (a > b) {\n    let maior = a;\n}  else {\n    let maior = b;\n}\nO maior número entre ' + a +  ' e ' + b + ' é o ' + maior);
+  alert('function maiorDeDois() {\n  let maior;\n  let a = 10;\n  let b = 5;\n\n  if (a > b) {\n    let maior = a;\n  } else {\n    let maior = b;\n  }\n  console.log(\'O maior número entre ' + a +  ' e ' + b + ' é o ' + maior + '\'\);\n}');
 }
 
-function maiorDeTres () {
+function maiorDeTres() {
   let maior;
   let a = 10;
   let b = 5;
@@ -53,5 +53,16 @@ function maiorDeTres () {
   } else {
     maior = c;
   }
-  alert('function maiorDeTres () {\n  let maior;\n  let a = 10;\n  let b = 5;\n  let c = 8;\n\n  if (a > b && a > c) {\n    maior = a;\n  } else if (b > c) {\n    maior = b;\n} else {\n  maior = c;\n}\nO maior número entre ' + a + ', ' + b + ' e ' + c + ' é o ' + maior);
+  alert('function maiorDeTres() {\n  let maior;\n  let a = 10;\n  let b = 5;\n  let c = 8;\n\n  if (a > b && a > c) {\n    maior = a;\n  } else if (b > c) {\n    maior = b;\n  } else {\n    maior = c;\n  }\n  console.log(\'O maior número entre \' + a + \' , \' + b + \' e \' + c + \' é o \' + maior);\n}');
+}
+
+function positiveNegativeZero() {
+  let number = 10;
+  let returnNum = 'zero';
+  if (number > 0) {
+    returnNum = 'positive';
+  } else if (number < 0) {
+    returnNum = 'negative';
+  }
+  alert('function positiveNegativeZero() {\n  let number = 10;\n  let returnNum = \'zero\';\n\n  if (number > 0) {\n    returnNum = \'positive\';\n  } else if (number < 0) {\n    returnNum = \'negative\'\n  }\n  console.log(\'O número é \' + returnNum);\n}')
 }
